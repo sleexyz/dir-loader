5f5bf418656a7e6f08ff2453583950a5d3e656b1
Sean Lee <freshdried@gmail.com>
2015-05-28 16:44:51 -0400

    changed index interface

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = require("./lib/index.js")
+var dir = require("./lib/dir-loader");
+var file = require("./lib/file-loader");
+var makeExport = require("./lib/make-export");
+var toExport = function(config) {
+    return makeExport(config);
+}
+
+toExport.loaders = {
+    dir: dir,
+    file: file
+};
+
+module.exports = toExport;
--- lib-src/make-export.js
+++ lib-src/make-export.js
@@ -1,21 +1,7 @@
-//TODO: clean, fix
-export default const build = function() {
-    const projectRoot = Promise.try(function getProjectRoot () {
-        return process.cwd();
-    });
+import Promise from "bluebird"
 
-    const config = projectRoot.then(function loadConfig(root) {
-        const config = require(root + "/fscms.config.js");
-        return config;
-    }).catch((e) => e.code === "MODULE_NOT_FOUND", function(e) {
-        console.log("fscms.config.js not found!");
-        process.exit(1);
-    }).catch(function(e) {
-        throw e;
-        process.exit(1);
-    });
-
-    config.then(function(config) {
+export default function(config) {
+    Promise.try(function(config) {
         // convert to k:v [k, v]
         return Object.keys(config.content).map((_) => [_, config.content[_]]);
     }).map(function(obj) { // convert
