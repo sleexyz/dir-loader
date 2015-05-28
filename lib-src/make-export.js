7fbbce55ab6910ba42427f404c380787e7192efa
Sean Lee <freshdried@gmail.com>
2015-05-28 16:55:47 -0400

    ported make-export to synchronous code

--- lib-src/make-export.js
+++ lib-src/make-export.js
@@ -1,21 +1,16 @@
 import Promise from "bluebird"
 
 export default function(config) {
-    Promise.try(function(config) {
-        // convert to k:v [k, v]
-        return Object.keys(config.content).map((_) => [_, config.content[_]]);
-    }).map(function(obj) { // convert
-        const [key, value] = obj;
-        const loader = value.loader
-        let options = value;
+    return Object.keys(config.content).map(
+            (_) => [_, config.content[_]]
 
-        return Promise.resolve(loader(options))
-            .then((str) => 
-                "exports['" + key + "'] = " + str + ";");
-    }).then(function(array) {
-        console.log(array.join("\n\n\n"));
-    }).catch((e) => e.code === "ENOENT", function(e) {
-        console.log(e.cause.path);
-        console.log("    was not found!");
-    });
+        ).map(function(obj) { // convert
+            const key = obj.key
+            const options = obj.options
+            const loader = options.loader
+            return [key, options.loader(options))];
+
+        }).map((kv) =>
+            "exports['" + kv[0] + "'] = " + kv[1] + ";")
+        ).join("\n\n\n");
 }
