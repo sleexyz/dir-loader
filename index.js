0035681cc23f88021ee15c76d6df786df67c0535
Sean Lee <freshdried@gmail.com>
2015-09-12 12:18:53 -0400

    got rid of file loader

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-var dirLoader = require("./lib/dir-loader");
-var fileLoader = require("./lib/file-loader");
 var makeExport = require("./lib/make-export");
 var toExport = function(config) {
     return makeExport(config);
 }
-
-toExport.loaders = {
-    dir: dirLoader,
-    file: fileLoader
-};
 
 module.exports = toExport;
